f3d39380a1913bac130ef4434fd1f281d66f6848
lyubenParashkevov <lubodp_1981@abv.bg>
2026-01-14 14:16:28 +0200

    Added DbSets and configured foreign keys with Fluent API in ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FootballStatistics.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FootballStatistics.Data
@@ -8,6 +9,31 @@ namespace FootballStatistics.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<League> Leagues { get; set; } = null!;
+        public DbSet<Team> Teams { get; set; } = null!;
+        public DbSet<Match> Matches { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           
+            modelBuilder
+                .Entity<Match>()
+                .HasOne(m => m.HomeTeam)
+                .WithMany(t => t.HomeMatches)
+                .HasForeignKey(m => m.HomeTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder
+                .Entity<Match>()
+                .HasOne(m => m.AwayTeam)
+                .WithMany(t => t.AwayMatches)
+                .HasForeignKey(m => m.AwayTeamId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
